ba37b43a715b457d803ea3b7ef5dcdc42f56fbf6
Ruben Lomo Maestu <ruben.lomomaestu@gmail.com>
2021-07-08 11:42:54 +0200

    template done within features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { AddTaskComponent } from './component/add-task/add-task.component';
+import { TasksComponent } from './component/tasks/tasks.component';
+import { TaskItemComponent } from './component/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AddTaskComponent,
+    TasksComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule
